9870e7d1228132c87837ddde756ed58e4bcce3ce
Asamin <CoffeeTaro@users.noreply.github.com>
2018-03-14 21:36:13 +0900

    add gitignore

--- build.sbt
+++ build.sbt
@@ -6,6 +6,6 @@ scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
 resolvers += "Atilika Open Source repository" at "http://www.atilika.org/nexus/content/repositories/atilika"
 libraryDependencies ++= Seq(
     "org.atilika.kuromoji" % "kuromoji" % "0.7.7",
-    "org.apache.spark" %% "spark-streaming" % "2.3.0" % "provided",
+    "org.apache.spark" % "spark-streaming_2.11" % "2.2.0",
     "org.apache.spark" %% "spark-streaming-twitter" % "1.6.3"
 )
--- src/main/scala/analysys.scala
+++ src/main/scala/analysys.scala
@@ -1,7 +1,18 @@
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
 import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
 
 object Main extends App{
+  // Sparkの初期設定
+  val conf = new SparkConf().setMaster("local").setAppName("geoTweetStream")
+  val sc = new SparkContext(sc)
+  val batchDuration = Seconds(3)
+  val ssc = new StreamingContext(sc, batchDuration)
+  val stream = TwitterUtils.createStream(ssc, None)
+
+  // 形態素解析
   val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
   val tokens = tokenizer.tokenize("ドはドーナッツのド").toArray
   tokens.foreach { t =>
