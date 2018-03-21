dd4e1bd7068d3ef173a66a178ca11354effb8639
Asamin <CoffeeTaro@users.noreply.github.com>
2018-03-21 21:06:34 +0900

    fix: 設定ファイル形式を変更

--- src/main/scala/analysys.scala
+++ src/main/scala/analysys.scala
@@ -1,3 +1,4 @@
+import com.typesafe.config._
 import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
@@ -5,16 +6,25 @@ import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
 
 object Main extends App{
-  // Sparkの初期設定
-  val conf = new SparkConf().setMaster("local[4]").setAppName("geoTweetStream")
-  val ssc = new StreamingContext(conf, Seconds(3))
-  val stream = TwitterUtils.createStream(ssc, None)
+  // Twitter Keysの読み込み
+  val config = ConfigFactory.load()
+  val consumerKey = config.getString("consumerKey")
+  val consumerSecret = config.getString("consumerSecret")
+  val accessToken = config.getString("accessToken")
+  val accessTokenSecret = config.getString("accessTokenSecret")
+  println(s"aaa")
+  println(accessTokenSecret)
 
-  // 形態素解析
-  val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
-  val tokens = tokenizer.tokenize("ドはドーナッツのド").toArray
-  tokens.foreach { t =>
-    val token = t.asInstanceOf[Token]
-    println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
-  }
+  // Sparkの初期設定
+  // val conf = new SparkConf().setMaster("local[4]").setAppName("geoTweetStream")
+  // val ssc = new StreamingContext(conf, Seconds(3))
+  // val stream = TwitterUtils.createStream(ssc, None)
+
+  // // 形態素解析
+  // val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+  // val tokens = tokenizer.tokenize("ドはドーナッツのド").toArray
+  // tokens.foreach { t =>
+  //   val token = t.asInstanceOf[Token]
+  //   println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
+  // }
 }
