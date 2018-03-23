12776ab81a321a719d4032867cf1614fae652fa9
kotaro asami <kotaro.a@uec.ac.jp>
2018-03-23 18:27:49 +0900

    change twitter-streaming module

--- src/main/scala/analysys.scala
+++ src/main/scala/analysys.scala
@@ -5,29 +5,31 @@ import org.apache.spark.streaming.twitter._
 import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
 
-object Main extends App{
+object Main {
   // Twitter Keysの読み込み
-  val config = ConfigFactory.load()
-  val consumerKey = config.getString("consumerKey")
-  val consumerSecret = config.getString("consumerSecret")
-  val accessToken = config.getString("accessToken")
-  val accessTokenSecret = config.getString("accessTokenSecret")
-  // Twitter Keysの設定
-  System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
-  System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
-  System.setProperty("twitter4j.oauth.accessToken", accessToken)
-  System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+  def main(args: Array[String]): Unit = {
+    val config = ConfigFactory.load()
+    val consumerKey = config.getString("consumerKey")
+    val consumerSecret = config.getString("consumerSecret")
+    val accessToken = config.getString("accessToken")
+    val accessTokenSecret = config.getString("accessTokenSecret")
+    // Twitter Keysの設定
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
 
-  // Sparkの初期設定
-  val conf = new SparkConf().setMaster("local[4]").setAppName("geoTweetStream")
-  val ssc = new StreamingContext(conf, Seconds(3))
-  val stream = TwitterUtils.createStream(ssc, None)
+    // Sparkの初期設定
+    val conf = new SparkConf().setMaster("local[4]").setAppName("geoTweetStream")
+    val ssc = new StreamingContext(conf, Seconds(3))
+    val stream = TwitterUtils.createStream(ssc, None)
 
-  // // 形態素解析
-  // val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
-  // val tokens = tokenizer.tokenize("ドはドーナッツのド").toArray
-  // tokens.foreach { t =>
-  //   val token = t.asInstanceOf[Token]
-  //   println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
-  // }
+    // 形態素解析
+    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+    val tokens = tokenizer.tokenize("ドはドーナッツのド").toArray
+    tokens.foreach { t =>
+      val token = t.asInstanceOf[Token]
+      println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
+    }
+  }
 }
